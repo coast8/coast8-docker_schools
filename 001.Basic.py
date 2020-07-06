935d31c49dc1cfc8dfafa3a9ca5f0f6bf5d58e22
Coast8 <smythy.costa@gmail.com>
2020-07-06 01:10:57 -0300

    update

--- 001.Basic.py
+++ 001.Basic.py
@@ -3,13 +3,13 @@
 ***************************************************************************************
 # 1.0 Comandos docker basicos
 ***************************************************************************************
-docker ps         										# listando containers ativos
-docker ps -a 											# listando todos os containers
-docker images     										# listando images
+docker ps         										                  # listando containers ativos
+docker ps -a 											                      # listando todos os containers
+docker images     										                  # listando images
 docker start CONTAINER_ID                               # startando um container
-docker stop CONTAINER_ID 								# finalizando o container
+docker stop CONTAINER_ID 								                # finalizando o container
 docker attach CONTAINER_ID                              # entrando no container
-docker container run --name mydeb -it debian bash 		# Renomeando Container
+docker container run --name mydeb -it debian bash 		  # Renomeando Container
 docker inspect CONTAINER_ID                             # inspecionando o container
 docker exec CONTAINER_ID comando_linux_bash             # executando um comando de fora do container
 docker status CONTAINER_ID                              # ver consumo de cpu
@@ -34,9 +34,9 @@ docker run -i -t ubuntu:14.10 /bin/bash
 # 3.0 Comandos linux basicos
 ***************************************************************************************
 
-uname -a                 			# venda a dist linux
-cat /etc/issue           			# venda a dist linux
-ps -ef                   			# vendo o process executed
+uname -a                 			    # venda a dist linux
+cat /etc/issue           			    # venda a dist linux
+ps -ef                   			    # vendo o process executed
 curl ip_container:porta_nginx 		# access nginx pelo host real
 
 
@@ -46,9 +46,9 @@ curl ip_container:porta_nginx 		# access nginx pelo host real
 ***************************************************************************************
 # 4.0 Saindo do container e voltando para o container
 ***************************************************************************************
-CTRL + D               			# encera, mata o container
-CTRL + P + Q           			# sai mais deixa o conteiner ativo
-docker ps 						# para pegar o CONTAINER_ID 
+CTRL + D               			    # encera, mata o container
+CTRL + P + Q           			    # sai mais deixa o conteiner ativo
+docker ps 						          # para pegar o CONTAINER_ID 
 docker attach CONTAINER_ID      # Voltando para o container
 docker diff CONTAINER_ID        # Verificando hitorico do container
 
